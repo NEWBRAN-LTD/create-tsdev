3f934816e65c55bc833f36cb1495ecd443fe8dc2
to1source-me <to1source@foxmail.com>
2021-05-26 22:33:48 +0800

    setup test folder

--- tests/dist.test.js
+++ tests/dist.test.js
@@ -1,9 +1,14 @@
 // this is a JS test the dist version
 // due to the fact that unit test passed but somehow the dist version failed
-
+const { join } = require('path')
 const test = require('ava')
 const { main } = require('../dist/main')
 
+const to = join(__dirname, 'fixtures', 'tmp-2')
+
 test(`Dist build file test`, t => {
-  t.pass()
+  return main({ to })
+    .then(() => {
+      t.pass()
+    })
 })
