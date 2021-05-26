b74bf89dbd82cb2f230ed130b149dc7ef9abb120
to1source-me <to1source@foxmail.com>
2021-05-26 22:40:31 +0800

    The dist test also passed so where is the tslib not found came from

--- tests/dist.test.js
+++ tests/dist.test.js
@@ -2,13 +2,25 @@
 // due to the fact that unit test passed but somehow the dist version failed
 const { join } = require('path')
 const test = require('ava')
+const fsx = require('fs-extra')
 const { main } = require('../dist/main')
 
-const to = join(__dirname, 'fixtures', 'tmp-2')
+const fixtures = join(__dirname, 'fixtures')
+const to = join(fixtures, 'tmp-2')
+
+test.before(() => {
+  fsx.copySync(join(fixtures, 'package-tpl.json'), join(to , 'package.json'))
+})
+
+test.after(() => {
+  fsx.removeSync(to)
+})
 
 test(`Dist build file test`, t => {
   return main({ to })
     .then(() => {
-      t.pass()
+      t.true(fsx.existsSync(join(to, 'clean.js')))
+      t.true(fsx.existsSync(join(to, 'src', 'main.ts')))
+      t.true(fsx.existsSync(join(to, 'tests', 'main.test.ts')))
     })
 })
