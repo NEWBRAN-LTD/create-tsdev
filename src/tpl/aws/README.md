5d303f6a57c94dfa74e0422cd3afa6e66f60395c
Joel Chu <joelckchu@gmail.com>
2021-06-02 14:44:25 +0800

    update the AWS readme to specify the objective

--- src/tpl/aws/README.md
+++ src/tpl/aws/README.md
@@ -1,3 +1,7 @@
 # AWS template
 
-this folder store all the AWS Lambda development files templates 
+this folder store all the AWS Lambda development files templates
+
+At this stage, we only support the backend function.
+
+Front end will have to be a separate call to this tool (develop in later day)
--- src/tpl/aws/credentials.ts
+++ src/tpl/aws/credentials.ts
@@ -6,9 +6,10 @@ import { writeFile } from 'fs-extra'
 
 /**
  * simple util to generate the aws required credential file and region file
+ * we only need one file call config 
  * @param {sring} key <ACCESS_KEY>
  * @param {string} secret <SECRET_ACCESS_KEY>
- * @param {string} region (optional) 
+ * @param {string} region (optional)
  */
 export function writeToCredentialFile(key: string, secret: string, region?: string): Promise<any> {
   const awsDir = join(homedir(), '.aws')
