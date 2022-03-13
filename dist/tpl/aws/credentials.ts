18b9f19df841cbbb2c5a0a9f70ebd560e406eeb8
Joel Chu <joelckchu@gmail.com>
2022-03-13 10:21:40 +0800

    fixing up the credentials file

--- dist/tpl/aws/credentials.ts
+++ dist/tpl/aws/credentials.ts
@@ -6,24 +6,25 @@ import { writeFile } from 'fs-extra'
 
 /**
  * simple util to generate the aws required credential file and region file
- * we only need one file call config 
+ * we only need one file call config
  * @param {sring} key <ACCESS_KEY>
  * @param {string} secret <SECRET_ACCESS_KEY>
  * @param {string} region (optional)
  */
 export function writeToCredentialFile(key: string, secret: string, region?: string): Promise<any> {
   const awsDir = join(homedir(), '.aws')
-  const filename = join(awsDir 'credentials')
+  const filename = join(awsDir, 'credentials')
   const content = `[default]
 aws_access_key_id = ${key}
 aws_secret_access_key = ${secret}
 `
   if (region) {
-    const regionFilename = join(awsDir, 'config')
-    const configContent = `[default]
+    const regionFilename: string = join(awsDir, 'config')
+    const configContent: string = `[default]
 region = ${region}
 `
     // @TODO
+    console.log(regionFilename, configContent)
   }
 
   return writeFile(filename, content)
